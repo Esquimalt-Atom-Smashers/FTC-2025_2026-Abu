4fe3386b24ea4c5f21886fbb778c3a99914822bd
BoscoTU <chungchaktu@gmail.com>
2025-10-13 17:20:55 -0700

    To be tested: relocate RR files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/TuningOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/TuningOpModes.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.roadrunner.tuning;
+package org.firstinspires.ftc.teamcode.roadrunner.tuning;
 
 import androidx.annotation.NonNull;
 
@@ -40,12 +40,12 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.OTOSLocalizer;
-import org.firstinspires.ftc.teamcode.PinpointLocalizer;
-import org.firstinspires.ftc.teamcode.TankDrive;
-import org.firstinspires.ftc.teamcode.ThreeDeadWheelLocalizer;
-import org.firstinspires.ftc.teamcode.TwoDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.OTOSLocalizer;
+import org.firstinspires.ftc.teamcode.roadrunner.PinpointLocalizer;
+import org.firstinspires.ftc.teamcode.roadrunner.TankDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.ThreeDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.roadrunner.TwoDeadWheelLocalizer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -68,60 +68,60 @@ public final class TuningOpModes {
                 .build();
     }
 
-//    private static PinpointView makePinpointView(PinpointLocalizer pl) {
-//        return new PinpointView() {
-//            GoBildaPinpointDriver.EncoderDirection parDirection = pl.initialParDirection;
-//            GoBildaPinpointDriver.EncoderDirection perpDirection = pl.initialPerpDirection;
-//
-//            @Override
-//            public void update() {
-//                pl.driver.update();
-//            }
-//
-//            @Override
-//            public int getParEncoderPosition() {
-//                return pl.driver.getEncoderX();
-//            }
-//
-//            @Override
-//            public int getPerpEncoderPosition() {
-//                return pl.driver.getEncoderY();
-//            }
-//
-//            @Override
-//            public float getHeadingVelocity(UnnormalizedAngleUnit unit) {
-//                return (float) pl.driver.getHeadingVelocity(unit);
-//            }
-//
-//            @Override
-//            public void setParDirection(@NonNull DcMotorSimple.Direction direction) {
-//                parDirection = direction == DcMotorSimple.Direction.FORWARD ?
-//                        GoBildaPinpointDriver.EncoderDirection.FORWARD :
-//                        GoBildaPinpointDriver.EncoderDirection.REVERSED;
-//                pl.driver.setEncoderDirections(parDirection, perpDirection);
-//            }
-//
-//            @Override
-//            public DcMotorSimple.Direction getParDirection() {
-//                return parDirection == GoBildaPinpointDriver.EncoderDirection.FORWARD ?
-//                        DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE;
-//            }
-//
-//            @Override
-//            public void setPerpDirection(@NonNull DcMotorSimple.Direction direction) {
-//                perpDirection = direction == DcMotorSimple.Direction.FORWARD ?
-//                        GoBildaPinpointDriver.EncoderDirection.FORWARD :
-//                        GoBildaPinpointDriver.EncoderDirection.REVERSED;
-//                pl.driver.setEncoderDirections(parDirection, perpDirection);
-//            }
-//
-//            @Override
-//            public DcMotorSimple.Direction getPerpDirection() {
-//                return perpDirection == GoBildaPinpointDriver.EncoderDirection.FORWARD ?
-//                        DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE;
-//            }
-//        };
-//    }
+    private static PinpointView makePinpointView(PinpointLocalizer pl) {
+        return new PinpointView() {
+            GoBildaPinpointDriver.EncoderDirection parDirection = pl.initialParDirection;
+            GoBildaPinpointDriver.EncoderDirection perpDirection = pl.initialPerpDirection;
+
+            @Override
+            public void update() {
+                pl.driver.update();
+            }
+
+            @Override
+            public int getParEncoderPosition() {
+                return pl.driver.getEncoderX();
+            }
+
+            @Override
+            public int getPerpEncoderPosition() {
+                return pl.driver.getEncoderY();
+            }
+
+            @Override
+            public float getHeadingVelocity() {
+                return (float) pl.driver.getHeadingVelocity(UnnormalizedAngleUnit.RADIANS);
+            }
+
+            @Override
+            public void setParDirection(@NonNull DcMotorSimple.Direction direction) {
+                parDirection = direction == DcMotorSimple.Direction.FORWARD ?
+                        GoBildaPinpointDriver.EncoderDirection.FORWARD :
+                        GoBildaPinpointDriver.EncoderDirection.REVERSED;
+                pl.driver.setEncoderDirections(parDirection, perpDirection);
+            }
+
+            @Override
+            public DcMotorSimple.Direction getParDirection() {
+                return parDirection == GoBildaPinpointDriver.EncoderDirection.FORWARD ?
+                        DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE;
+            }
+
+            @Override
+            public void setPerpDirection(@NonNull DcMotorSimple.Direction direction) {
+                perpDirection = direction == DcMotorSimple.Direction.FORWARD ?
+                        GoBildaPinpointDriver.EncoderDirection.FORWARD :
+                        GoBildaPinpointDriver.EncoderDirection.REVERSED;
+                pl.driver.setEncoderDirections(parDirection, perpDirection);
+            }
+
+            @Override
+            public DcMotorSimple.Direction getPerpDirection() {
+                return perpDirection == GoBildaPinpointDriver.EncoderDirection.FORWARD ?
+                        DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE;
+            }
+        };
+    }
 
     @OpModeRegistrar
     public static void register(OpModeManager manager) {
@@ -169,13 +169,12 @@ public final class TuningOpModes {
                     parEncs.add(new EncoderRef(0, 0));
                     perpEncs.add(new EncoderRef(0, 1));
                     lazyImu = new OTOSIMU(ol.otos);
-//                }  else if (md.localizer instanceof PinpointLocalizer) {
-//                    PinpointView pv = makePinpointView((PinpointLocalizer) md.localizer);
-//                    encoderGroups.add(new PinpointEncoderGroup(pv));
-//                    parEncs.add(new EncoderRef(0, 0));
-//                    perpEncs.add(new EncoderRef(0, 1));
-//                    lazyImu = new PinpointIMU(pv);
-//                }
+                }  else if (md.localizer instanceof PinpointLocalizer) {
+                    PinpointView pv = makePinpointView((PinpointLocalizer) md.localizer);
+                    encoderGroups.add(new PinpointEncoderGroup(pv));
+                    parEncs.add(new EncoderRef(0, 0));
+                    perpEncs.add(new EncoderRef(0, 1));
+                    lazyImu = new PinpointIMU(pv);
                 } else {
                     throw new RuntimeException("unknown localizer: " + md.localizer.getClass().getName());
                 }
@@ -247,12 +246,12 @@ public final class TuningOpModes {
                     ));
                     parEncs.add(new EncoderRef(0, 0));
                     perpEncs.add(new EncoderRef(0, 1));
-//                }  else if (td.localizer instanceof PinpointLocalizer) {
-//                    PinpointView pv = makePinpointView((PinpointLocalizer) td.localizer);
-//                    encoderGroups.add(new PinpointEncoderGroup(pv));
-//                    parEncs.add(new EncoderRef(0, 0));
-//                    perpEncs.add(new EncoderRef(0, 1));
-//                    lazyImu = new PinpointIMU(pv);
+                }  else if (td.localizer instanceof PinpointLocalizer) {
+                    PinpointView pv = makePinpointView((PinpointLocalizer) td.localizer);
+                    encoderGroups.add(new PinpointEncoderGroup(pv));
+                    parEncs.add(new EncoderRef(0, 0));
+                    perpEncs.add(new EncoderRef(0, 1));
+                    lazyImu = new PinpointIMU(pv);
                 } else if (td.localizer instanceof OTOSLocalizer) {
                     OTOSLocalizer ol = (OTOSLocalizer) td.localizer;
                     encoderGroups.add(new OTOSEncoderGroup(ol.otos));
@@ -297,9 +296,9 @@ public final class TuningOpModes {
         manager.register(metaForClass(MecanumMotorDirectionDebugger.class), new MecanumMotorDirectionDebugger(dvf));
         manager.register(metaForClass(DeadWheelDirectionDebugger.class), new DeadWheelDirectionDebugger(dvf));
 
-        manager.register(metaForClass(org.firstinspires.ftc.roadrunner.tuning.ManualFeedbackTuner.class), org.firstinspires.ftc.roadrunner.tuning.ManualFeedbackTuner.class);
-        manager.register(metaForClass(org.firstinspires.ftc.roadrunner.tuning.SplineTest.class), org.firstinspires.ftc.roadrunner.tuning.SplineTest.class);
-        manager.register(metaForClass(org.firstinspires.ftc.roadrunner.tuning.LocalizationTest.class), org.firstinspires.ftc.roadrunner.tuning.LocalizationTest.class);
+        manager.register(metaForClass(ManualFeedbackTuner.class), ManualFeedbackTuner.class);
+        manager.register(metaForClass(SplineTest.class), SplineTest.class);
+        manager.register(metaForClass(LocalizationTest.class), LocalizationTest.class);
 
         manager.register(metaForClass(OTOSAngularScalarTuner.class), new OTOSAngularScalarTuner(dvf));
         manager.register(metaForClass(OTOSLinearScalarTuner.class), new OTOSLinearScalarTuner(dvf));
@@ -313,7 +312,7 @@ public final class TuningOpModes {
                     LateralRampLogger.class,
                     ManualFeedforwardTuner.class,
                     MecanumMotorDirectionDebugger.class,
-                    org.firstinspires.ftc.roadrunner.tuning.ManualFeedbackTuner.class
+                    ManualFeedbackTuner.class
             )) {
                 configRoot.putVariable(c.getSimpleName(), ReflectionConfig.createVariableFromClass(c));
             }
