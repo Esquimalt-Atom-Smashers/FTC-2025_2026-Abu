c523f4b0b677d13bd2f4f4bc7790e7e61284b76d
BoscoTU <chungchaktu@gmail.com>
2025-11-13 23:03:10 -0800

    To be tested: block-java data passing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autos/ReadPosTeleOp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.opmode.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.utilities.RobotContainer;
+
+public class ReadPosTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        while (opModeIsActive()) {
+            RobotContainer.markReceived();
+
+            telemetry.addData("data is valid", RobotContainer.hasData());
+            telemetry.addData("x", RobotContainer.getX());
+            telemetry.addData("y", RobotContainer.getY());
+            telemetry.addData("heading", RobotContainer.getHeading());
+        }
+
+        RobotContainer.clear();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/PIDController.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+import org.firstinspires.ftc.robotcore.external.ExportToBlocks;
+
+public class PIDController extends BlocksOpModeCompanion {
+     @ExportToBlocks (
+         comment = "Magical number box that help a number meet another number",
+         tooltip = "enter and tune PID value",
+         parameterLabels = {"P", "I", "D"}
+     )
+
+    public static double calculate(double targetValue, double currentValue, double P, double I, double D) {
+         com.arcrobotics.ftclib.controller.PIDController pidController = new com.arcrobotics.ftclib.controller.PIDController(P, I, D);
+         return pidController.calculate(targetValue, currentValue);
+     }
+
+}
