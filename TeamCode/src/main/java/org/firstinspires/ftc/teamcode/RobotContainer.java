32aaf7762f02e0256d4655183b73299cc1c740b5
BoscoTU <chungchaktu@gmail.com>
2025-11-20 23:26:59 -0800

    To be tested: reset poses
    obviously a field is 144 inch wide (I have just realized that)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -6,8 +6,8 @@ import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
 import org.firstinspires.ftc.robotcore.external.ExportToBlocks;
 
 public class RobotContainer extends BlocksOpModeCompanion {
-    public static final Pose2d RED_RESET_POS = new Pose2d(60 - 11.5, 60 - 11, Math.toRadians(90));
-    public static final Pose2d BLUE_RESET_POS = new Pose2d(60 - 11.5, -(60 - 11), Math.toRadians(270));
+    public static final Pose2d RED_RESET_POS = new Pose2d(72 - 7.5, -(72 - 7.5), Math.toRadians(90));
+    public static final Pose2d BLUE_RESET_POS = new Pose2d(72 - 7.5, 72 - 7.5, Math.toRadians(270));
 
     public static double x = 0.0;
     public static double y = 0.0;
