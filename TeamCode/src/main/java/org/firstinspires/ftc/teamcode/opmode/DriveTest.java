2e44f54ce8a59a1d0162360945dcc88cd4635325
Esquimalt Atom Smashers 7287/20025 <esqatomsmashers@gmail.com>
2025-11-27 18:42:59 -0800

    To be tested: red teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveTest.java
@@ -33,7 +33,7 @@ public class DriveTest extends LinearOpMode {
         intakeFeedSubsystem = new IntakeFeedSubsystem(this);
         flywheelSubsystem = new FlywheelSubsystem(this);
         limelightSybsystem = new LimelightSybsystem(this, true);
-        driveSubsystem = new DriveSubsystem(this, new Pose2d(48,24,0));
+        driveSubsystem = new DriveSubsystem(this, new Pose2d((72 - 7),-(72 - 7),Math.toRadians(90fd)));
 
         buttonA = new PressAndReleaseButton();
         commandManager = new CommandManager(driveSubsystem, intakeFeedSubsystem, flywheelSubsystem, limelightSybsystem);
