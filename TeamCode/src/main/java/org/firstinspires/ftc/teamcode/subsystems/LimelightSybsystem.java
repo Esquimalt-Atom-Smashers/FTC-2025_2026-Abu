2b97e75c39a733ebbff6d9d7c69539fdfbf83c28
Bosco <bco445776@gmail.com>
2025-12-04 11:42:53 -0800

    To be completed: LL in onBot java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightSybsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightSybsystem.java
@@ -3,34 +3,37 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import android.sax.StartElementListener;
 
 import com.acmerobotics.roadrunner.Pose2d;
-import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+import org.firstinspires.ftc.robotcore.external.ExportToBlocks;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 import java.util.List;
 
-public class LimelightSybsystem extends SubsystemBase {
-    private Limelight3A limelight3A;
-    private String LIMELIGHT_NAME = "limelight";
+public class LimelightSybsystem extends BlocksOpModeCompanion {
+    private static Limelight3A limelight3A;
+    private static String LIMELIGHT_NAME = "limelight";
 
-    private final int RED_GOAL_APRILTAG_PIPELINE = 0;
-    private final int BLUE_GOAL_APRILTAG_PIPELINE = 1;
-    private final int PULL_RATE_HZ = 10;
+    private static final int RED_GOAL_APRILTAG_PIPELINE = 0;
+    private static final int BLUE_GOAL_APRILTAG_PIPELINE = 1;
+    private static final int PULL_RATE_HZ = 10;
 
     public final Pose2d RED_GOAL_POSE = new Pose2d(-60, 60 ,0);
     public final Pose2d BLUE_GOAL_POSE = new Pose2d(-60, -60, 0);
     private double METER_TO_INCH = 39.37008;
 
-    private boolean isRedAlliance;
+    private static boolean isRedAlliance;
     private OpMode opMode;
 
-    private Pose3D botPose;
+    private static Pose3D botPose;
     private double ty = 0.0;
     private int tagId = 0;
 
@@ -47,13 +50,48 @@ public class LimelightSybsystem extends SubsystemBase {
         }
     }
 
-    public boolean getIsRedAlliance() {return isRedAlliance;}
+    @ExportToBlocks(
+            comment = "",
+            tooltip = "initializeLimeLightSubsystem",
+            parameterLabels = {}
+    )
+    public static void initializeLimeLightSubsystem(boolean isOnRedAlliance) {
+        limelight3A = hardwareMap.get(Limelight3A.class, LIMELIGHT_NAME);
+        limelight3A.setPollRateHz(PULL_RATE_HZ);
+        limelight3A.start();
+        isRedAlliance = isOnRedAlliance;
+        if (isRedAlliance) {
+            limelight3A.pipelineSwitch(RED_GOAL_APRILTAG_PIPELINE);
+        } else {
+            limelight3A.pipelineSwitch(BLUE_GOAL_APRILTAG_PIPELINE);
+        }
+    }
+
+    @ExportToBlocks(
+            comment = "",
+            tooltip = "getIsRedAlliance",
+            parameterLabels = {}
+    )
+    public static boolean getIsRedAlliance() {return isRedAlliance;}
 
     public Pose2d getBotPose2D(Pose2d pose2d) {
         Pose2d returningPose = botPose != null? new Pose2d(botPose.getPosition().x * METER_TO_INCH, botPose.getPosition().y * METER_TO_INCH, Math.toRadians(180) + botPose.getOrientation().getYaw(AngleUnit.RADIANS)): pose2d;
         if (returningPose == pose2d) {return pose2d;}
         if (isRedAlliance) {
             returningPose = new Pose2d(-returningPose.position.x, returningPose.position.y, returningPose.heading.toDouble());
+        }
+        return returningPose;
+    }
+    @ExportToBlocks(
+            comment = "",
+            tooltip = "getBotPose2D",
+            parameterLabels = {"pose2D"}
+    )
+    public Pose2D getBotPose2D(Pose2D pose2d) {
+        Pose2D returningPose = botPose != null? new Pose2D(DistanceUnit.INCH, botPose.getPosition().x * METER_TO_INCH, botPose.getPosition().y * METER_TO_INCH, AngleUnit.RADIANS, Math.toRadians(180) + botPose.getOrientation().getYaw(AngleUnit.RADIANS)): pose2d;
+        if (returningPose == pose2d) {return pose2d;}
+        if (isRedAlliance) {
+            returningPose = new Pose2D(DistanceUnit.INCH, -botPose.getPosition().x * METER_TO_INCH, botPose.getPosition().y * METER_TO_INCH, AngleUnit.RADIANS, Math.toRadians(180) + botPose.getOrientation().getYaw(AngleUnit.RADIANS));
         }
         return returningPose;
     }
@@ -77,27 +115,36 @@ public class LimelightSybsystem extends SubsystemBase {
         return ty;
     }
 
-    public void periodic() {
+    @ExportToBlocks(
+            comment = "",
+            tooltip = "update Limelight",
+            parameterLabels = {}
+    )
+    public static void periodic() {
         LLResult result = limelight3A.getLatestResult();
         if (result.isValid()) {
-            ty = result.getTy();
+//            ty = result.getTy();
             botPose = result.getBotpose();
             List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
-            for (LLResultTypes.FiducialResult fr : fiducialResults) {
-                if (fr.getFiducialId() == 21 || fr.getFiducialId() == 22 || fr.getFiducialId() == 33){
-                    tagId = fr.getFiducialId();
-                }
-            }
-            opMode.telemetry.addLine("Limelight got data");
+//            for (LLResultTypes.FiducialResult fr : fiducialResults) {
+//                if (fr.getFiducialId() == 21 || fr.getFiducialId() == 22 || fr.getFiducialId() == 33){
+//                    tagId = fr.getFiducialId();
+//                }
+//            }
+            telemetry.addLine("Limelight got data");
         } else {
-            ty = -999.0;
+//            ty = -999.0;
             botPose = null;
-            tagId = 0;
-            opMode.telemetry.addLine("Limelight no data");
+//            tagId = 0;
+            telemetry.addLine("Limelight no data");
         }
     }
-
-    public void stop() {
+    @ExportToBlocks(
+            comment = "",
+            tooltip = "stop",
+            parameterLabels = {}
+    )
+    public static void stop() {
         limelight3A.stop();
     }
 }
