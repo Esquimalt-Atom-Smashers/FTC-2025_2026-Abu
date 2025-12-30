85aa5d63c3183d7aa710b0ce5c6e93f6a4494374
Bosco <bco445776@gmail.com>
2025-12-30 14:45:32 -0800

    Works: tidy up unused auto reading code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autos/AutoFromTextFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autos/AutoFromTextFile.java
@@ -4,17 +4,15 @@ import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.ActiveAutoSequence;
 import org.firstinspires.ftc.teamcode.RobotPropertyParser;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 
 @Autonomous(name = "Experimental: AutoFromTextFile")
@@ -38,25 +36,25 @@ public class AutoFromTextFile extends LinearOpMode {
         //Get starting Position
         String startingPosTxt = actions.get(0);
         switch (startingPosTxt) {
-            case "BLUE_FAR":
+            case "BLUE.FAR":
                 startingPos = new Pose2d(72 - 7, -16, Math.toRadians(180));
-                telemetry.addLine("starting at BLUE_FAR");
+                telemetry.addLine("starting at BLUE.FAR");
                 break;
-            case "BLUE_CLOSE":
+            case "BLUE.CLOSE":
                 startingPos = new Pose2d(-72 + 21.5, -72 + 17.5, Math.toRadians(143.65));
-                telemetry.addLine("starting at BLUE_CLOSE");
+                telemetry.addLine("starting at BLUE.CLOSE");
                 break;
-            case "RED_FAR":
+            case "RED.FAR":
                 startingPos = new Pose2d(65,16, Math.toRadians(180));
-                telemetry.addLine("starting at RED_FAR");
+                telemetry.addLine("starting at RED.FAR");
                 break;
-            case "RED_CLOSE":
+            case "RED.CLOSE":
                 startingPos = new Pose2d(-72 + 19.5, 72 - 18, Math.toRadians(53.5));
-                telemetry.addLine("starting at RED_CLOSE");
+                telemetry.addLine("starting at RED.CLOSE");
                 break;
-            case "TESTING_MODE":
+            case "TESTING.MODE":
                 startingPos = new Pose2d(0, 0, 0);
-                telemetry.addLine("starting at MIDDLE_OF_FIELD(testing mode)");
+                telemetry.addLine("starting at MIDDLE.OF.FIELD(testing mode)");
                 break;
             default:
                 try {
@@ -70,13 +68,19 @@ public class AutoFromTextFile extends LinearOpMode {
         //cycle through the actions to add them to the roadrunner action array
         for (String action: actions){
             switch (action) {
-                case "TO_POS1":
-                    actionList.add(goToPos1Action());
-                    telemetry.addLine("action to pos 1");
+                case "RED.FAR.SHOOT":
+                    actionList.add(redFarShootAction());
+                    telemetry.addLine("RED.FAR.SHOOT");
                     break;
-                case "TO_POS2":
-                    actionList.add(goToPos2Action());
-                    telemetry.addLine("action to pos 2");
+                case "RED.THIRD.INTAKE":
+                    actionList.add(redThirdIntakeAction());
+                    telemetry.addLine("RED.THIRD.INTAKE");
+                    break;
+                case "DELAY.HUNDRED.MS":
+                    actionList.add(new SleepAction(0.1));
+                    break;
+                case "DELAY.ONE.S":
+                    actionList.add(new SleepAction(1));
                     break;
                 default:
                     printAction("default liner");
@@ -92,26 +96,20 @@ public class AutoFromTextFile extends LinearOpMode {
         Actions.runBlocking(actionSequence);
     }
 
-    private Action goToPos1Action() {
+    private Action redFarShootAction() {
         return driveSubsystem.getMecanumDrive().actionBuilder(driveSubsystem.getCurrentPos()).
-                strafeToLinearHeading(new Vector2d(10, 0), Math.toRadians(90)).build();
-//        return new InstantAction(() -> telemetry.addLine("action1 go to pos 1"));
+                strafeToLinearHeading(new Vector2d(55, 18), Math.toRadians(72)).build();
     }
 
-    private Action goToPos2Action() {
-        return driveSubsystem.getMecanumDrive().actionBuilder(driveSubsystem.getCurrentPos()).
-                strafeToLinearHeading(new Vector2d(0, 10), Math.toRadians(0)).build();
-//        return new InstantAction(() -> telemetry.addLine("action2 go to pos 2"));
-
+    private Action redThirdIntakeAction() {
+        return driveSubsystem.getMecanumDrive().actionBuilder(driveSubsystem.getCurrentPos())
+                .strafeToLinearHeading(new Vector2d(40, 30), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(40, 43), Math.toRadians(90))
+                .build();
     }
     private Action printAction(String line) {
-//        return driveSubsystem.getMecanumDrive().actionBuilder(driveSubsystem.getCurrentPos()).
-//                strafeToLinearHeading(new Vector2d(0, 10), Math.toRadians(0)).build();
         return new InstantAction(() -> telemetry.addLine(line));
 
     }
-
-    private Action printNum(int i) {
-        return new InstantAction(() -> telemetry.addData("i=",i));
-    }
 }
+
