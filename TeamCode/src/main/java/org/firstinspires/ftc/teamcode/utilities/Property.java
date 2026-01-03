885139a92321d7a05445fc191fec9ea6facc8004
Bosco <bco445776@gmail.com>
2026-01-03 14:45:38 -0800

    Works: redFarShootAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Property.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Property.java
@@ -24,32 +24,32 @@ public class Property {
     public static volatile double BLUE_CLOSE_Y = -72 + 17.5;
     public static volatile double BLUE_CLOSE_HEADING = 143.65;
 
-    public static volatile double FAR_SHOOT_RPM = 3500;
+    public static volatile double FAR_SHOOT_RPM = 3375;
     public static volatile double CLOSE_SHOOT_RPM = 3100;
 
     public static volatile double RED_FAR_SHOOT_X = 55;
-    public static volatile double RED_FAR_SHOOT_Y = 18;
-    public static volatile double RED_FAR_SHOOT_HEADING = 72;
+    public static volatile double RED_FAR_SHOOT_Y = 17;
+    public static volatile double RED_FAR_SHOOT_HEADING = 74;
 
-    public static volatile double RED_THIRD_INTAKE_P1_X = 35;
+    public static volatile double RED_THIRD_INTAKE_P1_X = 38;
     public static volatile double RED_THIRD_INTAKE_P1_Y = 30;
     public static volatile double RED_THIRD_INTAKE_P1_HEADING = 90;
 
-    public static volatile double RED_THIRD_INTAKE_P2_X = 35;
-    public static volatile double RED_THIRD_INTAKE_P2_Y = 43;
+    public static volatile double RED_THIRD_INTAKE_P2_X = 38;
+    public static volatile double RED_THIRD_INTAKE_P2_Y = 55;
     public static volatile double RED_THIRD_INTAKE_P2_HEADING = 90;
 
-    public static volatile double RED_LOAD_INTAKE_P1_X = 44;
-    public static volatile double RED_LOAD_INTAKE_P1_Y = 55;
-    public static volatile double RED_LOAD_INTAKE_P1_HEADING = 0;
+    public static volatile double RED_LOAD_INTAKE_P1_X = 48;
+    public static volatile double RED_LOAD_INTAKE_P1_Y = 65;
+    public static volatile double RED_LOAD_INTAKE_P1_HEADING = 22.5;
 
-    public static volatile double RED_LOAD_INTAKE_P2_X = 60;
-    public static volatile double RED_LOAD_INTAKE_P2_Y = 55;
+    public static volatile double RED_LOAD_INTAKE_P2_X = 70;
+    public static volatile double RED_LOAD_INTAKE_P2_Y = 65;
     public static volatile double RED_LOAD_INTAKE_P2_HEADING = 0;
 
     //flywheel control
     public static volatile double TOLERANCE = 100;
     public static volatile double kV = 0.000215;
 
-    public static volatile double SHOOTING_SECONDS = 3.0;
+    public static volatile double SHOOTING_SECONDS = 5.0;
 }
