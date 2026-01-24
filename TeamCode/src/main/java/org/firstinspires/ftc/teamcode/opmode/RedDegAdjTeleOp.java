f6a6a822149be6f4bd4b503e2d541574eb6597f5
Bosco <bco445776@gmail.com>
2026-01-24 10:34:11 -0800

    works: adj deg in teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/RedDegAdjTeleOp.java
@@ -0,0 +1,121 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeTransferSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.VisionSubsystem;
+import org.firstinspires.ftc.teamcode.utilities.InputUtility;
+import org.firstinspires.ftc.teamcode.utilities.Property;
+import org.firstinspires.ftc.teamcode.utilities.RobotContainer;
+import org.firstinspires.ftc.teamcode.utilities.RobotPositionHolder;
+import org.firstinspires.ftc.teamcode.utilities.RobotPropertyParser;
+
+@Config
+@TeleOp(name = "Kenny Red deg adjust TeleOp", group = "AAA")
+public class RedDegAdjTeleOp extends LinearOpMode {
+    RobotContainer robotContainer;
+    Pose2d startingPose = new Pose2d(Property.TELEOP_DEFAULT_STARTING_POS_X, Property.TELEOP_DEFAULT_STARTING_POS_Y, Math.toRadians(Property.TELEOP_DEFAULT_STARTING_POS_HEADING));
+    RobotContainer.Alliance alliance = RobotContainer.Alliance.RED;
+    double targetRpm;
+    ShooterSubsystem.FlywheelSetting flywheelSetting = new ShooterSubsystem.FlywheelSetting(targetRpm, 0);
+    boolean isManualRPMControl = true;
+    boolean isLeftDpadPressed = false;
+    boolean isRightDpadPressed = false;  
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotPropertyParser.loadTeleOp();
+        targetRpm = Property.CLOSE_SHOOT_RPM;
+        if (RobotPositionHolder.hasData()) {
+            startingPose = new Pose2d(RobotPositionHolder.getX(), RobotPositionHolder.getY(), RobotPositionHolder.getHeading());
+        }
+        robotContainer = new RobotContainer(this,
+                startingPose,
+                alliance,
+                DriveSubsystem.DriveSubsystemState.TELEOP_DRIVING,
+                ShooterSubsystem.ShooterState.MANUAL,
+                IntakeTransferSubsystem.IntakeTransferState.DISABLED,
+                VisionSubsystem.VisionState.TRACKING_GOAL);
+        robotContainer.drivebase.setDriveHeadingErrorTo(Math.toRadians(90));
+        if (RobotPositionHolder.hasData()) {
+            RobotPositionHolder.markReceived();
+        }
+        waitForStart();
+        while (opModeIsActive()) {
+            boolean forceReset = false;
+            boolean isLLReseting = false;
+            //drive control
+            double drive = InputUtility.deadZoneJoyStick(-gamepad1.left_stick_y);
+            double strafe = InputUtility.deadZoneJoyStick(-gamepad1.left_stick_x);
+            double turn = InputUtility.deadZoneJoyStick(-gamepad1.right_stick_x);
+
+            //reset field centric
+            if (gamepad1.start || gamepad1.share) {
+                robotContainer.drivebase.setHeading(alliance == RobotContainer.Alliance.RED? 90: 270);
+            }
+            //reset aimbot
+            if (gamepad1.back || gamepad1.share) {
+                robotContainer.updatePoseFromVision(true);
+            }
+
+            //shooting && intake control
+            if (gamepad1.right_trigger >= 0.3) {
+                robotContainer.intake.setState(IntakeTransferSubsystem.IntakeTransferState.FEEDING_SHOOTER);
+            } else if (gamepad1.right_bumper) {
+                robotContainer.intake.setState(IntakeTransferSubsystem.IntakeTransferState.EJECTING);
+            } else {
+                robotContainer.intake.setState(IntakeTransferSubsystem.IntakeTransferState.INTAKING);
+            }
+
+            //flywheel control
+            if (gamepad1.triangle) {
+                isManualRPMControl = false;
+                forceReset = true;
+            } else if (gamepad1.square) {
+                isManualRPMControl = true;
+                forceReset = false;
+            }
+
+            if (isManualRPMControl) {
+                if (gamepad1.dpad_up) {
+                    targetRpm = Property.FAR_SHOOT_RPM;
+                } else if (gamepad1.dpad_down) {
+                    targetRpm = Property.CLOSE_SHOOT_RPM;
+                }
+            }
+
+            if (!gamepad1.dpad_left && isLeftDpadPressed) {
+                robotContainer.drivebase.setHeading(Math.toDegrees(robotContainer.getPose().heading.toDouble()) + 1);
+                isLeftDpadPressed = false;
+            } else if (!gamepad1.dpad_right && isRightDpadPressed) {
+                robotContainer.drivebase.setHeading(Math.toDegrees(robotContainer.getPose().heading.toDouble()) - 1);
+                isRightDpadPressed = false;
+            } else if (gamepad1.dpad_left && !isLeftDpadPressed) {
+                isLeftDpadPressed = true;
+            } else if (gamepad1.dpad_right && !isRightDpadPressed) {
+                isRightDpadPressed = true;
+            }
+            robotContainer.shoot(targetRpm, isManualRPMControl);
+            if (gamepad1.left_trigger >= 0.3) {
+                robotContainer.aimbotAssistedDrive(drive, strafe, turn);
+            } else {
+                robotContainer.drive(drive, strafe, turn);
+            }
+            if (gamepad1.x) {
+                robotContainer.updatePoseFromVision(forceReset);
+                isLLReseting = true;
+            }
+            robotContainer.addOpModeTelemetry("isManualRPM: " + isManualRPMControl +"\nisReseting Manual: " + isLLReseting);
+            robotContainer.runRobot();
+        }
+        robotContainer.shutDownRobot();
+        RobotPositionHolder.clear();
+        Property.reset();
+    }
+}
